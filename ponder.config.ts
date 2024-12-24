bc152f7b5373a8333d11fe3a64105a29f8f9b99e
Simonas Karuzas <simonas@not.cat>
2024-12-24 08:51:18 +0200

    feat: contract config

--- ponder.config.ts
+++ ponder.config.ts
@@ -4,18 +4,17 @@ import { erc721ABI } from "./abis/erc721ABI";
 
 export default createConfig({
   networks: {
-    arbitrum: {
-      chainId: 42161,
-      transport: http(process.env.PONDER_RPC_URL_42161),
+    mainnet: {
+      chainId: 1,
+      transport: http(process.env.PONDER_RPC_URL_1),
     },
   },
   contracts: {
     ERC721: {
-      network: "arbitrum",
+      network: "mainnet",
       abi: erc721ABI,
-      address: "0x6325439389E0797Ab35752B4F43a14C004f22A9c",
-      startBlock: 3163146,
-      endBlock: 3200000,
+      address: "0x7aB2F10CaC6E27971fa93A5D5470Bb84126Bb734",
+      startBlock: 19337083,
     },
   },
 });
